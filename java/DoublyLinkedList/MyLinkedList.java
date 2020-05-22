311c1507023981ef3f83e0df58a890f6cffab742
Jeongsik Mun <farfs.dev@gmail.com>
2020-05-22 20:30:15 +0900

    Java - Doubly Linked List

--- java/DoublyLinkedList/MyLinkedList.java
+++ java/DoublyLinkedList/MyLinkedList.java
@@ -1,30 +1,26 @@
-// https://leetcode.com/explore/learn/card/linked-list/209/singly-linked-list/1290/
+// https://leetcode.com/explore/learn/card/linked-list/210/doubly-linked-list/1294/
 
 public class MyLinkedList {
+    DoublyListNode head = null;
 
-    private SinglyLinkedNode head = null;
-
-    static class SinglyLinkedNode {
+    // Definition for doubly-linked list.
+    static class DoublyListNode {
         int val;
-        SinglyLinkedNode next = null;
-
-        SinglyLinkedNode(int v) {
-            val = v;
-        }
+        DoublyListNode next, prev;
+        DoublyListNode(int x) {val = x;}
     }
 
     /** Initialize your data structure here. */
     public MyLinkedList() {
     }
 
-    /** Get the value of the index-th node in the linked list.
-     * If the index is invalid, return -1. */
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
     public int get(int index) {
         if (head == null) {
             return -1;
         }
 
-        SinglyLinkedNode cur = head;
+        DoublyListNode cur = head;
         for (int i = 0; i < index; i++) {
             if (cur.next == null) {
                 return -1;
@@ -36,46 +32,47 @@ public class MyLinkedList {
     }
 
     /** Add a node of value val before the first element of the linked list.
-     * After the insertion, the new node will be the first node of the linked list */
+     * After the insertion, the new node will be the first node of the linked list. */
     public void addAtHead(int val) {
         if (head == null) {
-            head = new SinglyLinkedNode(val);
+            head = new DoublyListNode(val);
             return;
         }
 
-        SinglyLinkedNode node = new SinglyLinkedNode(val);
+        DoublyListNode node = new DoublyListNode(val);
         node.next = head;
+        head.prev = node;
         head = node;
     }
 
     /** Append a node of value val to the last element of the linked list. */
     public void addAtTail(int val) {
         if (head == null) {
-            head = new SinglyLinkedNode(val);
+            head = new DoublyListNode(val);
             return;
         }
 
-        SinglyLinkedNode cur = head;
+        DoublyListNode cur = head;
         while (cur.next != null) {
             cur = cur.next;
         }
-        SinglyLinkedNode node = new SinglyLinkedNode(val);
+        DoublyListNode node = new DoublyListNode(val);
         cur.next = node;
+        node.prev = cur;
     }
 
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list,
-     * the node will be appended to the end of linked list.
-     * If index is greater than the length, the node will not be inserted. */
+     * the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
     public void addAtIndex(int index, int val) {
         if (head == null) {
             if (index == 0) {
-                head = new SinglyLinkedNode(val);
+                head = new DoublyListNode(val);
             }
             return;
         }
 
-        SinglyLinkedNode prev = null;
-        SinglyLinkedNode cur = head;
+        DoublyListNode prev = null;
+        DoublyListNode cur = head;
         for (int i = 0; i < index; i++) {
             if (cur == null) {
                 return;
@@ -85,16 +82,20 @@ public class MyLinkedList {
             cur = cur.next;
         }
 
-        SinglyLinkedNode node = new SinglyLinkedNode(val);
+        DoublyListNode node = new DoublyListNode(val);
         if (prev == null) {
             node.next = head;
+            head.prev = node;
             head = node;
         } else if (cur == null) {
             cur = node;
             prev.next = cur;
+            cur.prev = prev;
         } else {
             prev.next = node;
+            node.prev = prev;
             node.next = cur;
+            cur.prev = node;
         }
     }
 
@@ -104,24 +105,24 @@ public class MyLinkedList {
             return;
         }
 
-        SinglyLinkedNode prev = null;
-        SinglyLinkedNode cur = head;
+        DoublyListNode cur = head;
         for (int i = 0; i < index; i++) {
             if (cur.next == null) {
                 return;
             }
             // Move to Index (i + 1)
-            prev = cur;
             cur = cur.next;
         }
 
-        SinglyLinkedNode next = cur.next;
+        DoublyListNode prev = cur.prev;
+        DoublyListNode next = cur.next;
         if (prev == null) {
-            head = head.next;
+            head = next;
         } else if (next == null) {
             prev.next = null;
         } else {
             prev.next = next;
+            next.prev = prev;
         }
     }
 }
